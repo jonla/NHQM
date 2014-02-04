a22bd661811ef5838c2fdbf05d746dd9e405246c
David <davlid@student.chalmers.se>
2014-02-04 19:05:13 +0000

    code modified

--- scripts/testhelium5.py
+++ scripts/testhelium5.py
@@ -4,12 +4,12 @@ Created on 2 feb 2014
 @author: David Lidberg
 '''
 from imports import *
-import solve
-import quantum_numbers
-import bases.momentum
-from problems import Helium5
-from helpers.quantum import absq
-from helpers.plotting import find_resonance_state
+import nhqm.solve
+import nhqm.quantum_numbers
+import nhqm.bases.momentum
+from nhqm.problems import Helium5
+from nhqm.helpers.quantum import absq
+from nhqm.helpers.plotting import find_resonance_state
 
 problem=Helium5()
 basis_state_count = 20 # What does this do? Is this how we discretize k?
@@ -18,33 +18,33 @@ l = 0
 j = 0.5
 x_peak=0.17  # Determines peak of the triangle
 y_peak=-0.07
-quantum_numbers = quantum_numbers.QuantumNumbers(l, j)
+quantum_numbers = nhqm.quantum_numbers.QuantumNumbers(l, j)
 
 '''
 Triangular basis, so the contour contains resonance states. Is mom_tri a berggren-basis now?
 '''
-contour_tri = bases.momentum.triangle_contour(x_peak, y_peak, k_max, basis_state_count, 5)
-mom_tri = bases.momentum.MomentumBasis(contour_tri) 
+contour_tri = nhqm.bases.momentum.triangle_contour(x_peak, y_peak, k_max, basis_state_count, 5)
+mom_tri = nhqm.bases.momentum.MomentumBasis(contour_tri) 
 
 
 
 '''
 Same values but with the normal contour
 '''
-contour_str = bases.momentum.gauss_contour([0, k_max], basis_state_count)
-mom_str = bases.momentum.MomentumBasis(contour_str)
+contour_str = nhqm.bases.momentum.gauss_contour([0, k_max], basis_state_count)
+mom_str = nhqm.bases.momentum.MomentumBasis(contour_str)
 
 
 
 
-r=sp.linspace(0,10,100)
+r=sp.linspace(0,50,500)
 
 '''
 Solve med de olika baserna. resonance tycks alltid vara samma som ground_state_tri[0]. Vet inte vad det betyder.
 Hursomhelst, jag tror att triangelkonturen skapar en berggren-bas
 '''
-states_tri=solve.solve(problem, quantum_numbers, mom_tri)
-states_str=solve.solve(problem, quantum_numbers, mom_str)
+states_tri=nhqm.solve.solve(problem, quantum_numbers, mom_tri)
+states_str=nhqm.solve.solve(problem, quantum_numbers, mom_str)
 
 
 resonance_tri = find_resonance_state(states_tri) 
