536450ef2f87b453d1a7fd83d13916dcefd2e6ad
hjelmare <hjelmare@student.chalmers.se>
2014-02-19 17:45:56 +0100

    Just playing around

--- scripts/gh_test.py
+++ scripts/gh_test.py
@@ -4,21 +4,8 @@ My random stuff... sometimes useful, most often not...
 
 from __future__ import division
 from numpy import * 
+from eigensolvers.largest import largestEig
 
-def largestEig(A, its=10):
-    "Finds largest eigenpair of matrix using power iterations"
-
-    D = A.size**.5  # get size of input matrix
-
-    v = random.rand(D)  # random starting guess
-
-    for i in range(its):    # power iterations
-        v = dot(A,v)
-        v = v*linalg.norm(v)**-1
-    
-    theta = linalg.norm(dot(A,v))
-    
-    return [theta, v]
 
 D = 100
 
