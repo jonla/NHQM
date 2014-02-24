b159303bc4185383615d0927abd2db292877eb52
David <davlid@student.chalmers.se>
2014-02-24 13:40:57 +0000

    JOCC

--- scripts/eigensolvers/run.py
+++ scripts/eigensolvers/run.py
@@ -10,25 +10,26 @@ The resaults can be compared using numpys eig function.
 
 k = 100         # Matrix size
 TOL = 1.e-1     # Margin of error
-D = 200           # Size of diagonal shape
+D = 100        # Size of diagonal shape
 N = 5000        # Number of itterations for Jacobi
 
 A = matrixgen(k, D)
 
 e, w = powersolve(A, TOL)
+Jac = jacobi_eigensolver(np.asmatrix(A), N)
 Pow = np.sort(e)
-print "Cumputed eigenvalues using Power"
-print Pow
+print "Computed eigenvalues using Power"
+print Pow[0]
 
-D = jacobi_eigensolver(np.asmatrix(A), N)
-Jac = np.sort(np.diag(D))
+
+
 print "Computed eigenvalues using Jacobi"
-print Jac
+print Jac[0]
 
-eig, vec = np.linalg.eig(A)
+eig= np.linalg.eigvals(A)
 Eig = np.sort(eig)
 print "Computed eigenvalues using eig"
-print Eig
+print Eig[0]
 
 print "Standard error Power:", np.std(Eig - Pow)
 print "Standard error Jacobi:", np.std(Eig - Jac)
